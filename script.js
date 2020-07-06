55bfe7d10e4d62e980af874396c6ab1e2d6b9d45
Adrian Marquis <adrian.marquis78@gmail.com>
2020-07-06 15:12:03 -0400

    Full Reboot

--- script.js
+++ script.js
@@ -2,33 +2,21 @@
  
 const videoSearchURL = 'https://www.googleapis.com/youtube/v3/search';
 const googleApiKey = 'AIzaSyCxrK1XHc-SVjAMhKBwz6-Z0oZCvZrXk-A';
-const edamamApiURL = 'https://api.edamam.com/api/food-database/v2';
-const edamamId = '347aa534';
-const edamamApiKey = '775483288483a637da90fb07604782c7';
-let food = []; //map and join, think STORE databank from quiz app
-
 
 //Calculators:
-
-function generateBMR() {
+function calculateBMR() {
   console.log("BMR calculated");
   if ($(".gender") === "male") {
     return ($(".weight") * 0.453592) * 10 + ($(".height") * 2.54) * 6.25 - $(".age") * 5 + 5
   } else {
-    return ($(".weight") * 0.453592) * 10 + ($(".height") * 2.54) * 6.25 - $(".age") * 5 + 161 
+    return ($(".weight") * 0.453592) * 10 + ($(".height") * 2.54) * 6.25 - $(".age") * 5 + 161
   }
 }
 
-//Will this operate to sum all the calories?
-function calculateCalories() {
-  const calories = responseJson.calories; //need to acquire 'calories' from API and sum
-  return responseJson.ingr.filter(food => ingr.calories).val();
-}
-
 function generateCalorieBurn() {
-  const caloricDeficit = calculateCalories() - generateBMR();
+  const caloricDeficit = $(".calorie-query") - calculateBMR();
+  return `${caloricDeficit}`
 }
-
 
 
 //Page Generators:
@@ -37,25 +25,15 @@ function generateStartPage() {
       <p>Enter your gender, height, weight, and age to yield your BMR, or
           basal metabolic rate (how many calories you burn in a day just 
           sitting still).</p> 
-      <p>Then enter food items or your daily calorie
-          intake to calculate a "goal caloric deficit" based against your BMR.</p>
+      <p>Then enter your daily calorie intake to calculate a "goal caloric deficit" 
+          based against your BMR.</p>
       <p>Workout videos you can do at home will display or follow one of the 
           links below them to do something different!</p>
       `
 }
 
 function generateInputPage(food) {
-    const foodList = `
-              <ul class="food-list">
-                  ${food.map((foodItem, index) => 
-                    `<li class="food-item"><label for="food${index}">
-                      ${foodItem}
-                    </label></li>
-                    `)
-                  .join("\n")
-                }
-              </ul>
-              `
+      const displayBMR = `$`
       return `
       <form class="bmr-form">
         <fieldset name="user-search">
@@ -78,17 +56,7 @@ function generateInputPage(food) {
               <button type="submit" >Calculate BMR</button>
         </fieldset>
       </form>
-      
-      <form class="food-form">
-        <fieldset name="food-search">
-          <legend>Enter Your Food:</legend>
-            <input type="text" class="food-query" placeholder="e.g. 1 cup Cheerios with 1 cup 
-            skim milk and half banana" required>
-            <button type="submit">Add Food</button>
-              ${foodList}
-            <img src="images/edamam-img.png" id="logo" alt="edamam logo">
-        </fieldset>      
-      </form>
+
       <form class="calorie-form">
         <fieldset>
           <legend>Enter Your Daily Calories:</legend>
@@ -97,11 +65,7 @@ function generateInputPage(food) {
         </fieldset>
       </form>
       `
-    /*2nd form: user adds foods or daily calories and submits for total calories --> calculate BMR --> 
-calculate caloricDeficit. Double 'required' inputs? Or one or the other?*/
 }
-
-
 
 //Server request formatting:
 function formatQueryParams(params) {
@@ -110,35 +74,6 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-
-
-//Edamam Data:
-function getFood(foodId) {
-  const headers = {
-    app_id: edamamId,
-    app_key: edamamApiKey,
-    "Access-Control-Allow-Origin": "https://schism578.github.io/api-hack/"
-  }
-  const params = {
-    ingr: "",
-    calories: ""
-  };
-
-  //Can this be duplicated like this for both Edamam and YouTube fetches?
-  const queryString = formatQueryParams(params);
-  const foodURL = edamamApiURL + '?' + queryString;
-
-  return fetch(foodURL, {headers})
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  })
-  .catch(err => {
-    $('#error-message').text(`Something went wrong with Nutritionix: ${err.message}`);
-  });
-}
 
 //YouTube Data:
 function displayVideoResults(responseJson) {
@@ -153,7 +88,7 @@ function displayVideoResults(responseJson) {
       `//add 'other' links
     )};
   $('#results').removeClass('hidden');
-};
+}
 
 function getVideos(caloricDeficit, maxResults=3) {
   const params = {
@@ -163,7 +98,7 @@ function getVideos(caloricDeficit, maxResults=3) {
     maxResults,
     type: 'video',
     list: 'exercise'
-  };
+  }
 
   const queryString = formatQueryParams(params)
   const videoURL = videoSearchURL + '?' + queryString;
@@ -191,7 +126,7 @@ function displayStartPage() {
 
 function displayInputPage() {
   console.log("displaying page")
-  displayPage(generateInputPage(food))
+  displayPage(generateInputPage())
 }
 
 function displayPage(html) {
@@ -212,32 +147,24 @@ function handleSubmitBMR() {
     event.preventDefault();
     const male = $(".weight") * 10 + $(".height") * 6.25 - $(".age") * 5 + 5;
     const female = $(".weight") * 10 + $(".height") * 6.25 - $(".age") * 5 + 161;
-    return `<span>${generateBMR() ? male : female}</span>`
+    return `<span>${calculateBMR(gender) ? male : female}</span>`
   });
 }
 
-function handleSubmitFood() {
-  $('main').on("submit", ".food-form", event => {
-    event.preventDefault();
-    const foodQuery = $(".food-query")
-    const foodName = foodQuery.val()
-    foodQuery.val("")
-    getFood(foodName).then(responseJson => console.dir(responseJson))
-  });
-}
-
-function handleSubmitCalorie() {
+function handleSubmitCalories() {
   $('main').on("submit", ".calorie-form", event => {
     event.preventDefault();
-    
+    const calorieQuery = $(".calorie-query");
+    const calories = calorieQuery.val();
+    generateCalorieBurn(calories);
+    displayVideoResults();
   });
 }
 
 function setupEventHandlers() {
   handleClickStart()
   handleSubmitBMR()
-  handleSubmitFood()
-  handleSubmitCalorie()
+  handleSubmitCalories()
 }
 
 $(setupEventHandlers);
